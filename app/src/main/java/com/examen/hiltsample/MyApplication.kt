c5a512035bf864020398c358b2b52e2fc8c9e4af
Sainathhiwale <sainathhiwale2@gmail.com>
2025-05-25 22:18:26 +0530

    Hilt sample repo is created

---
+++ app/src/main/java/com/examen/hiltsample/MyApplication.kt
@@ -0,0 +1,15 @@
+package com.examen.hiltsample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+class MyApplication:Application() {
+     @Inject
+     lateinit var userRepository:UserRepository
+    override fun onCreate() {
+        super.onCreate()
+        userRepository.sendEmail("sainath@gmai.com","pass1234")
+    }
+}
---
+++ app/src/main/java/com/examen/hiltsample/UserRepository.kt
@@ -0,0 +1,11 @@
+package com.examen.hiltsample
+
+import android.util.Log
+import javax.inject.Inject
+
+class UserRepository @Inject constructor() {
+     val TAG = "UserRepository"
+    fun sendEmail(userName:String,password:String){
+        Log.d(TAG, "sendEmail: ")
+    }
+}
